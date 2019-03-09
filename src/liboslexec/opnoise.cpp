f0c478528dc696aaa4b6adc6890baf585944c693
rzulak <rzulak@imageworks.com>
2019-03-08 21:04:46 -0800

    Remove redundant declarations

--- src/liboslexec/opnoise.cpp
+++ src/liboslexec/opnoise.cpp
@@ -39,14 +39,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 OSL_NAMESPACE_ENTER
 namespace pvt {
 
-#ifdef __CUDA_ARCH__
-namespace OSLDeviceStrings {
-#define STRDECL(str,var_name)                       \
-    extern __device__ OSL_NAMESPACE::DeviceString var_name;
-#include <OSL/strdecls.h>
-#undef STRDECL
-}
-#endif
 
 #if 0 // only when testing the statistics of perlin noise to normalize the range
 
