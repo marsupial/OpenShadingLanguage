8315758db35786c95460d5f5c24260e31cc5a7fc
rzulak <rzulak@imageworks.com>
2019-05-03 13:12:52 -0700

    extern template

--- src/liboslexec/shadingsys.cpp
+++ src/liboslexec/shadingsys.cpp
@@ -69,16 +69,16 @@ using namespace OSL::pvt;
 OSL_NAMESPACE_ENTER
 
 namespace pvt {
-template <> OSL_HOSTDEVICE Color3
+template <> Color3
 ColorSystem::transformc<Color3> (StringParam fromspace, StringParam tospace, const Color3& C, Context);
 
-template <> OSL_HOSTDEVICE Dual2<Color3>
+template <> Dual2<Color3>
 ColorSystem::transformc<Dual2<Color3>> (StringParam fromspace, StringParam tospace, const Dual2<Color3>& C, Context);
 
-template <> OSL_HOSTDEVICE Color3
+template <> Color3
 ColorSystem::ocio_transform<Color3> (StringParam fromspace, StringParam tospace, const Color3& C, Context);
 
-template <> OSL_HOSTDEVICE Dual2<Color3>
+template <> Dual2<Color3>
 ColorSystem::ocio_transform<Dual2<Color3>> (StringParam fromspace, StringParam tospace, const Dual2<Color3>& C, Context);
 }
 
